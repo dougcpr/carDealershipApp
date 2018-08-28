a6c476a2438dbf6f6ebdff6f8684ea097c1d02b1
Doug Cooper <doug.cpr@gmail.com>
2018-08-28 11:11:52 -0500

    updated test for mocking cars with sunroofs request

--- src/app/services/carListings/carListings.service.spec.ts
+++ src/app/services/carListings/carListings.service.spec.ts
@@ -32,4 +32,19 @@ describe('CarListingsService', () => {
         req.flush({data: CARS});
       })
   );
+
+  it('expects service to cars that have sunroofs with POST',
+    inject([HttpTestingController, CarListingsService],
+      (httpMock: HttpTestingController, service: CarListingsService) => {
+        // We call the service
+        service.searchForAllMatchingResults({hasSunroof: true}).subscribe(data => {
+          expect(data).toBe(data);
+        });
+        // We set the expectations for the HttpClient mock
+        const req = httpMock.expectOne(`${route}/searchAll`);
+        expect(req.request.method).toEqual('POST');
+        // Then we set the fake data to be returned by the mock
+        req.flush({data: CARS});
+      })
+  );
 });
